783c60fc4a2a0db495308a3c69b689662b048d45
seu-catarinabarbosa <catarina.pinheiro07@gmail.com>
2019-08-21 14:34:29 -0300

    Adição de funções para cifrar o código e decifrar o código, com modificações de nomeclatura de HTML, e modificações no index,js para funcionalidade de innerHTML

--- src/cipher.js
+++ src/cipher.js
@@ -1,14 +1,37 @@
+/* eslint-disable for-direction */
 // Adicionar funções de cifrar e decifrar
+// Funções do Dani
 
 window.cipher = {
   encode: encode,
   decode: decode
 };
+// encoda a mensagem com o descol
+function encode(offset,inputEncode) {
+  let messageForInput;
+  let messageCharCode;
+  let resultEncode;
 
-function encode(){
+  for (let i= 0; i < inputEncode.length; i++) {
+    messageForInput = inputEncode[i].charCodeAt();
+    messageCharCode = ((messageForInput - 32 + offset) % 108 + 32);
+    resultEncode += String.fromCharCode(messageCharCode);
+  }
+  return resultEncode ;
 
-};
+}
 
-function decode(){
+// desencoda a mensagem com o descol
+function decode(offset,inputDecode) {
+  let messageForInputDecode;
+  let messageCharCode;
+  let resultDecode;
 
-};
+  for (let i = 0; i < inputDecode.length; i++) {
+    messageForInputDecode = inputDecode[i].charCodeAt();
+    messageCharCode = ((messageForInputDecode - 32 - offset) % 108 + 32);
+    resultDecode += String.fromCharCode(messageCharCode);
+  }
+
+  return resultDecode ;
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-// Funcionalidade dos botões
+// Funcionalidade dos botões pegar dados do HTML
 
-function nameOfFunction(){
-  window.cipher.encode ();
-};
+function giveEncode() {
+  let offset = Number(document.getElementById("offset").value);
+  let inputEncode= document.getElementById("inputEncode").value;
+  let resultEncode = cipher.encode(offset,inputEncode);
+  document.getElementById("resultEncode").innerHTML= `<p> Sua mensagem codificada é ${resultEncode} </p>`;
+
+}
+
+function giveDecode() {
+  let offset = Number(document.getElementById("offset").value);
+  let inputDecode = document.getElementById("inputDecode").value;
+  let resultDecode = cipher.decode(offset, inputDecode);
+  document.getElementById("resultDecode").innerHTML= `<p> Sua mensagem decodificada é ${resultDecode} </p>`;
+
+}
