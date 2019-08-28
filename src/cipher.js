f020cfb914163dda34e52b4ef28f4da09dbbc1c4
seu-catarinabarbosa <catarina.pinheiro07@gmail.com>
2019-08-28 15:14:49 -0300

    adição de caracteries especiais, letras minusculas, rodando o teste 100%

--- src/cipher.js
+++ src/cipher.js
@@ -11,11 +11,18 @@ function encode(offsetEncode, inputEncode) {
   let resultEncode = "";
 
   for (let i = 0; i < inputEncode.length; i++) {
-    let messageForInput = inputEncode[i].charCodeAt();
-    let messageCharCode = ((messageForInput - 65 + offsetEncode) % 26) + 65;
-    resultEncode += String.fromCharCode(messageCharCode);
+    let messageForInputEncode = inputEncode[i].charCodeAt();
+    let messageCharCode = ((messageForInputEncode - 65 + offsetEncode) % 26) + 65;
+    if (messageForInputEncode >= 65 && messageForInputEncode <= 90) {
+      resultEncode += String.fromCharCode(messageCharCode);
+    } else if (messageForInputEncode >=97 && messageForInputEncode<= 122) {
+      let messageCharCodeTiny = ((messageForInputEncode - 97 + offsetEncode) % 26) + 97;
+      resultEncode += String.fromCharCode(messageCharCodeTiny);
+    } else {
+      let messageCharEncodeSpecial = ((messageForInputEncode - 32 + offsetEncode) % 33) + 32;
+      resultEncode += String.fromCharCode(messageCharEncodeSpecial);
+    }
   }
-
   return resultEncode;
 }
 
@@ -25,9 +32,16 @@ function decode(offsetDecode, inputDecode) {
 
   for (let i = 0; i < inputDecode.length; i++) {
     let messageForInputDecode = inputDecode[i].charCodeAt();
-    let messageCharCode = ((messageForInputDecode - 90 - offsetDecode) % 26) + 90;
-    resultDecode += String.fromCharCode(messageCharCode);
+    let messageCharDecode = ((messageForInputDecode - 90 - offsetDecode) % 26) + 90;
+    if (messageForInputDecode >= 65 && messageForInputDecode <= 90) {
+      resultDecode += String.fromCharCode(messageCharDecode);
+    } else if (messageForInputDecode >=97 && messageForInputDecode <= 122) {
+      let messageCharCodeTiny = ((messageForInputDecode - 122 - offsetDecode) % 26) + 122;
+      resultDecode += String.fromCharCode(messageCharCodeTiny);
+    } else {
+      let messageCharDecodeSpecial = ((messageForInputDecode - 64 - offsetDecode) % 33) + 64;
+      resultDecode += String.fromCharCode(messageCharDecodeSpecial);
+    }
   }
-
   return resultDecode;
 }
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -15,7 +15,7 @@ describe("cipher", () => {
       );
     });
 
-    // Hacker edition
+    //Hacker edition
     //
     // [Español]
     // Si decides agregar soporte para minúsculas descomenta el test a
@@ -23,28 +23,28 @@ describe("cipher", () => {
     //
     // [Português]
     // Se quiser adicionar testes para letras minúsculas, descomente o teste
-    // abaixo.
-    //
-    // it("should return "hijklmnopqrstuvwxyzabcdefg" for "abcdefghijklmnopqrstuvwxyz" with offset 33", () => {
-    //   assert.equal(
-    //     cipher.encode(33, "abcdefghijklmnopqrstuvwxyz"),
-    //     "hijklmnopqrstuvwxyzabcdefg"
-    //   );
-    // });
+    //abaixo.
+
+    it("should return hijklmnopqrstuvwxyzabcdefg for abcdefghijklmnopqrstuvwxyz with offset 33", () => {
+      assert.equal(
+        cipher.encode(33, "abcdefghijklmnopqrstuvwxyz"),
+        "hijklmnopqrstuvwxyzabcdefg"
+      );
+    });
 
     // Hacker edition
     //
     // [Español]
     // Si decides implementar soporte para caracteres no alfabéticos descomenta
-    // el test a continuación.
-    //
-    // [Português]
+    //el test a continuación.
+
+    //Português]
     // Se quiser adicionar testes para caracteres não alfabéticos, descomente o
-    // teste abaixo.
-    //
-    // it("should return " !@" for " !@"", () => {
-    //   assert.equal(cipher.encode(33, " !@")," !@");
-    // });
+    //teste abaixo.
+
+    it("should return ' !@' for ' !@'", () => {
+      assert.equal(cipher.encode(33, " !@"), " !@");
+    });
   });
 
   describe("cipher.decode", () => {
@@ -70,12 +70,12 @@ describe("cipher", () => {
     // Se quiser adicionar testes para letras minúsculas, descomente o teste
     // abaixo.
     //
-    // it("should return "abcdefghijklmnopqrstuvwxyz" for "hijklmnopqrstuvwxyzabcdefg" with offset 33", () => {
-    //   assert.equal(
-    //     cipher.decode(33, "hijklmnopqrstuvwxyzabcdefg"),
-    //     "abcdefghijklmnopqrstuvwxyz"
-    //   );
-    // });
+    it("should return 'abcdefghijklmnopqrstuvwxyz' for 'hijklmnopqrstuvwxyzabcdefg' with offset 33", () => {
+      assert.equal(
+        cipher.decode(33, "hijklmnopqrstuvwxyzabcdefg"),
+        "abcdefghijklmnopqrstuvwxyz"
+      );
+    });
 
     // Hacker edition
     //
@@ -87,8 +87,8 @@ describe("cipher", () => {
     // Se quiser adicionar testes para caracteres não alfabéticos, descomente o
     // teste abaixo.
     //
-    // it("should return " !@" para " !@"", () => {
-    //   assert.equal(cipher.decode(33, " !@")," !@");
-    // });
+    it("should return ' !@' para ' !@'", () => {
+      assert.equal(cipher.decode(33, " !@"), " !@");
+    });
   });
 });
